49fdf1893149c4fe4a2f2e4df99fc928bf276799
Philippe Chartrand <philippe.chartrand@videotron.ca>
2017-11-02 20:53:36 -0400

    script d'aggrégation des données passées

---
+++ aggregate.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import glob
+import json
+
+
+def consolidate(pattern):
+    sparse = {}
+    consolidated = []
+    outfilename = '{}.json'.format(pattern)
+    for file in glob.glob("{}*2017*.json".format(pattern)):
+        print(file)
+        with open(file) as jsonfile:
+            data = json.load(jsonfile)
+            for i, serie in enumerate(data):
+                print(i)
+                if not i in sparse:
+                    sparse[i] = {}
+                for point in serie:
+                    timestamp = point['date']
+                    sparse[i][timestamp] = point['value']
+
+    for i in sparse:
+        newlist = []
+        for date in sorted(sparse[i].keys()):
+            newlist.append(dict(date=date, value=sparse[i][date]))
+        consolidated.append(newlist)
+
+    with open(outfilename,'w') as outfile:
+        print('output in {}'.format(outfilename))
+        json.dump(consolidated,outfile)
+
+
+if __name__ == '__main__':
+    consolidate('monthly')
+    consolidate('weekly')
